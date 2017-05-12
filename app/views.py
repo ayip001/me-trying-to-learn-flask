d0e8eaf28b199097db9d31638564064e33e9ea8f
Angus Yip <ayip002@gmail.com>
2017-05-12 13:38:24 -0700

    templates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,21 @@
+from flask import render_template
 from app import app
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'placeholder user object'} # placeholder user object
+    posts = [
+        {
+            'author': {'nickname': 'friend user 1'},
+            'body': 'post 1 lorem ipsum'
+        },
+        {
+            'author': {'nickname': 'friend user 2'},
+            'body': 'post 2 foo bar'
+        }
+    ]
+    return render_template('index.html',
+                           title = 'Home',
+                           user = user,
+                           posts = posts)
