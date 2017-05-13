de5c61e703bab4e75a5bdd35d822ff371ff2a024
Angus Yip <ayip002@gmail.com>
2017-05-12 17:01:23 -0700

    basic form views

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# me-trying-to-learn-flask" 
+"# me-trying-to-learn-flask"
+
+
+pip install Flask-WTF
+pip install flask
--- app/views.py
+++ app/views.py
@@ -1,21 +1,13 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname': 'placeholder user object'} # placeholder user object
-    posts = [
-        {
-            'author': {'nickname': 'friend user 1'},
-            'body': 'post 1 lorem ipsum'
-        },
-        {
-            'author': {'nickname': 'friend user 2'},
-            'body': 'post 2 foo bar'
-        }
-    ]
-    return render_template('index.html',
-                           title = 'Home',
-                           user = user,
-                           posts = posts)
+# @app.route('/')
+# @app.route('/index')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html',
+                           title = 'Sign in',
+                           form = form)
